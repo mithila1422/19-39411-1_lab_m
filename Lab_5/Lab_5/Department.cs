3cc58878fc98a330f945e325ad01fab7b5f63c61
Insana Mithila <mithilaislam900@gmail.com>
2021-03-01 12:17:03 +0600

    Lab_5

---
+++ Lab_5/Lab_5/Department.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    class Department
+    {
+        
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+             
+        private string id;
+
+        public string Id
+             {
+            get  {return id;}
+               set  {id=value;}
+             }
+        private Student[] students;
+
+        public void AddStudent (Student s)
+        {
+            students [studentCount++] = s;
+            
+        
+        }
+        public void PrintStudent()
+        {
+            for (int i = 0; i < studentCount; i++)
+            {
+                students[i].ShowInfo();
+            }
+        }
+        public Student GetStudent(string id)
+        {
+            return null;
+        }
+        public int studentCount;
+        public int StudentCount
+        {
+            get { return studentCount; }
+            set { studentCount = value; }
+        }
+
+       public Department(){
+           students = new Student[50];
+       }
+
+        public Department(string name,string id){
+    this.name=name;
+        this.id=id;
+        students = new Student[50];
+    
+    }
+
+    public void ShowInfo(){
+    Console.WriteLine("Name:"+name);
+    Console.WriteLine("Id:"+id);
+   
+    }
+
+    }
+}
---
+++ Lab_5/Lab_5/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student ("Methela", "193411",3.5f );
+            Student s1 = new Student("Meth", "193412", 3.55f);
+            Student s2 = new Student("Mim", "193413", 3.61f);
+
+            Department d = new Department("Computer Science", "CS");
+            Department d1 = new Department("Electrical Engineering", "EEE");
+            s.Dept = d;
+            d.AddStudent(s);
+            d.AddStudent(s1);
+            s1.Dept = d;
+            s2.Dept = d1;
+            d.AddStudent(s2);
+
+            s.ShowDdepartmentDetails();
+            s2.ShowDdepartmentDetails();
+            Console. Writeline("*****");
+            d.Name = "Computer Science and Engineering";
+            d1.Id = "CSE";
+                 s1.ShowDdepartmentDetails();
+             Console.WriteLine("*****");
+            d.PrintStudent();
+        }
+    }
+}
